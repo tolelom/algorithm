7b7eb1f254b9515cf2b2f3e1e95087bdd92e438a
김성민 <35192253+tolelom@users.noreply.github.com>
2020-09-05 17:49:48 +0900

    Update segmemt.cpp

--- segmemt.cpp
+++ segmemt.cpp
@@ -5,30 +5,37 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
-typedef vector<ll> vll;
+
+/*
+root index = 1
+parent's index = i
+child's index = i*2, i*2+1
+*/
+
+
 
 class Segment {  // segment tree class
-    ll n;
+    ll nItem;
     ll half;
-    vll item;
+    vector<ll> item;
 
    public:
-    Segment(ll n) : n(n) {  // 생성자
-        for (half = 1; half < n; half <<= 1)
+    Segment(ll size) : nItem(size) {  // 생성자
+        for (half = 1; half < size; half <<= 1)
             ;
         item.resize(half * 2);  // n보다 큰 2의 제곱 수
     }
 
     void init(ll n) { // 초기화 함수
         fill(item.begin(), item.end(), 0);
-        for (ll i = 0; i < n; ++i)  // 0 ~ n-1까지
+        for (ll i = 0; i < n; ++i)  // half ~ half + n-1까지
             cin >> item[i + half];
 
         for (ll i = half - 1; i; --i)  // 조건을 i로 하는 흑마법
             item[i] = item[i * 2] + (item[i * 2 + 1]);
     }
 
-    ll sum(ll left, ll right) {
+    ll sum(ll left, ll right) { // left ~ right 번째 수의 합을 구하는 함수
         ll res = 0;
         left += half - 1, right += half - 1;
         while (left <= right) {
